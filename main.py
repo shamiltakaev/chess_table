81cb913745ed4b0705053e0c0e772c8fae9c6dda
shamiltakaev <shamilchechya@gmail.com>
2023-12-09 17:37:44 +0300

    Add timer

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ if __name__ == '__main__':
     pygame.init()
     size = width, height = 1000, 1000
     clock = pygame.time.Clock()
-    fps = 10
+    fps = 50
 
     screen = pygame.display.set_mode(size)
-    life = Life(10, 10)
-    life.set_view(100, 100, 50)
+    life = Life(30, 30)
+    life.set_view(100, 100, 20)
+    speed = 300
+    MYEVENTTYPE = pygame.USEREVENT + 1
+    pygame.time.set_timer(MYEVENTTYPE, speed)
+    t_speed = speed
     running = True
     is_play = False
     while running:
@@ -19,14 +23,25 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                life.get_click(event.pos)
+
+                if event.button == 1:
+                    life.get_click(event.pos)
+                elif event.button == 4:
+                    speed //= 2
+                elif event.button == 5:
+                    speed *= 2
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     is_play = not is_play
+            if event.type == MYEVENTTYPE:
+                if is_play:
+                    life.next_move()
         screen.fill((0, 0, 0))
+        if t_speed != speed:
+            t_speed = speed
+            pygame.time.set_timer(MYEVENTTYPE, speed)
+
         life.render(screen)
-        if is_play:
-            life.next_move()
         clock.tick(fps)
 
         pygame.display.flip()
