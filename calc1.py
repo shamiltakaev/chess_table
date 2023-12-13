7e4c716481d6ba2c4c5642a744d762d78a6b620c
shamiltakaev <shamilchechya@gmail.com>
2023-12-13 17:41:45 +0300

    Edit file calc1

--- calc1.py
+++ calc1.py
@@ -35,36 +35,32 @@ class Creature(pygame.sprite.Sprite):
 
     def update(self, *args, **kwargs):
         if args:
-            if args[1][pygame.K_LEFT]:
+            if args[0][pygame.K_LEFT]:
                 self.rect.x -= 10
-            if args[1][pygame.K_RIGHT]:
+            if args[0][pygame.K_RIGHT]:
                 self.rect.x += 10
-            if args[1][pygame.K_UP]:
+            if args[0][pygame.K_UP]:
                 self.rect.y -= 10
-            if args[1][pygame.K_DOWN]:
+            if args[0][pygame.K_DOWN]:
                 self.rect.y += 10
             
         return super().update(*args, **kwargs)
 
 
 if __name__ == '__main__':
-
     pygame.mouse.set_visible(False)
-
     clock = pygame.time.Clock()
     running = True
     all_sprites = pygame.sprite.Group()
     Creature(all_sprites)
-    tick = pygame.USEREVENT
-    pygame.time.set_timer(tick, 300)
 
     while running:
         screen.fill((255, 255, 255))
         keys = pygame.key.get_pressed()
         all_sprites.draw(screen)
+        if any(keys):
+            all_sprites.update(keys)
         for event in pygame.event.get():
-            if any(keys):
-                all_sprites.update(event, keys)
             if event.type == pygame.QUIT:
                 running = False
         pygame.display.flip()
--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 import pygame
-from board import Board
-from minesweeper import Minesweeper
+
+import os
+import sys
+
+
+pygame.init()
+size = width, height = 800, 400
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    image = pygame.transform.scale(image, (60, 60))
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Creature(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.image = load_image("arrow.png")
+        self.rect = self.image.get_rect()
+
+    def update(self, *args, **kwargs):
+        if args:
+            if args[0][pygame.K_LEFT]:
+                self.rect.x -= 10
+            if args[0][pygame.K_RIGHT]:
+                self.rect.x += 10
+            if args[0][pygame.K_UP]:
+                self.rect.y -= 10
+            if args[0][pygame.K_DOWN]:
+                self.rect.y += 10
+            
+        return super().update(*args, **kwargs)
 
 
 if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode((600, 600))
-    fps = 50
+    pygame.mouse.set_visible(False)
     clock = pygame.time.Clock()
     running = True
-    N = 15
-    board = Minesweeper(N, N, 10)
-    
-    while True:
-        screen.fill(pygame.Color("black"))
+    all_sprites = pygame.sprite.Group()
+    Creature(all_sprites)
+
+    while running:
+        screen.fill((255, 255, 255))
+        keys = pygame.key.get_pressed()
+        all_sprites.draw(screen)
+        if any(keys):
+            all_sprites.update(keys)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                board.get_click(event.pos)
-        screen.fill((0, 0, 0))
-        board.render(screen)
+                running = False
         pygame.display.flip()
-        clock.tick()
+        clock.tick(100)
+
+    pygame.quit()
